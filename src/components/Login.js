244c4735a0482ecf09215795e631d02fac0735cc
shriansh agarwal <shrianshpbt@gmail.com>
2021-04-24 16:35:25 +0530

    partially build

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,9 +7,8 @@ const Login = (props) => {
                 <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
                 <SignUp>GET ALL THERE</SignUp>
                 <Description>
-                    Get Premier Access to Raya and the Last Dragon for an
-                    additional fee with a Disney+ subscription. As of 03/26/21
-                    the price of Disney+ and the Disney Bundle will increase by $10.
+                    Made With ❤ by Shriansh Agarwal
+                    
                 </Description>
                 <CTALogoTwo src="/images/cta-logo-two.png" alt="" />
             </CTA>
