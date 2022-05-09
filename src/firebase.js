c379f359dd25994679416cd9b3ce24bb2413e555
Shriansh Agarwal <41548480+ShrianshCodingTutorials@users.noreply.github.com>
2022-05-09 23:12:54 +0530

    added env-files

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBWoSNL4XP6Vf9s2hTllD536vxrW3tMQWM",
-    authDomain: "disneyplus-clone-3c066.firebaseapp.com",
-    projectId: "disneyplus-clone-3c066",
-    storageBucket: "disneyplus-clone-3c066.appspot.com",
-    messagingSenderId: "465593547753",
-    appId: "1:465593547753:web:5ec6638662db1e9efb2978",
-    measurementId: "G-JGTCB3QX3Z"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+
 };
 
 // Initialize Firebase
